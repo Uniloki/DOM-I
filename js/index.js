91191525aa620db0669c39f914813935ccadf900
unknown <uniloki009@gmail.com>
2020-06-09 14:04:12 -0400

    finished task 2 and 1 onto task 3 and stretch'

--- js/index.js
+++ js/index.js
@@ -48,6 +48,54 @@ navList[2].textContent = siteContent.nav["nav-item-3"];
 navList[3].textContent = siteContent.nav["nav-item-4"];
 navList[4].textContent = siteContent.nav["nav-item-5"];
 navList[5].textContent = siteContent.nav["nav-item-6"];
+
+
+
+
 //CTA HEADER
 const ctaHeader = document.querySelector('.cta-text h1')
 ctaHeader.textContent = siteContent.cta.h1;
+//CTA BUTTON
+const ctaButton = document.querySelector('.cta-text button')
+ctaButton.textContent = siteContent.cta.button;
+//CTA IMAGE
+const ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent.cta['img-src']);
+
+//MAIN CONTENT
+//MAIN CONTENT TOP
+const headList = document.querySelectorAll('.text-content h4');
+headList[0].textContent = siteContent['main-content']['features-h4'];
+headList[1].textContent = siteContent['main-content']['about-h4'];
+
+const paraList = document.querySelectorAll('.text-content p');
+paraList[0].textContent = siteContent['main-content']['features-content'];
+paraList[1].textContent = siteContent['main-content']['about-content'];
+
+//MAIN CONTENT IMAGE
+const mainContentImg = document.getElementById('middle-img');
+mainContentImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//MAIN CONTENT BOTTOM
+headList[2].textContent = siteContent['main-content']['services-h4'];
+headList[3].textContent = siteContent['main-content']['product-h4'];
+headList[4].textContent = siteContent['main-content']['vision-h4'];
+paraList[2].textContent = siteContent['main-content']['services-content'];
+paraList[3].textContent = siteContent['main-content']['product-content'];
+paraList[4].textContent = siteContent['main-content']['vision-content'];
+
+
+//CONTACT
+
+const contactHead = document.querySelector('.contact h4');
+const contactPara = document.querySelectorAll('.contact p');
+
+contactHead.textContent = siteContent['contact']['contact-h4'];
+contactPara[0].textContent = siteContent['contact']['address'];
+contactPara[1].textContent = siteContent['contact']['phone'];
+contactPara[2].textContent = siteContent['contact']['email'];
+
+//FOOTER
+
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
